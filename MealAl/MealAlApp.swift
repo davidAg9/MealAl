7c2e55792e45d321c4a81aa48f615281304d2d12
davidAg9 <shane_alpha9@outlook.com>
2021-08-11 23:31:50 +0000

    Initial Commit

---
+++ MealAl/MealAlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAlApp.swift
+//  MealAl
+//
+//  Created by Dave Agyakwa on 11/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MealAlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
